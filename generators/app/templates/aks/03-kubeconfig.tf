330575ea787446aa9769865aa12a2baa74fc592f
Rohith Teja Gundimeda <rohithteja95@outlook.com>
2023-06-13 16:19:01 +0530

    Override kubecontext

--- generators/app/templates/aks/03-kubeconfig.tf
+++ generators/app/templates/aks/03-kubeconfig.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "kubectl" {
   provisioner "local-exec" {
-    command = "az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.cluster_name}"
+    command = "az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.cluster_name} --context --overwrite-existing"
   }
   depends_on = [
     azurerm_kubernetes_cluster.aks,
