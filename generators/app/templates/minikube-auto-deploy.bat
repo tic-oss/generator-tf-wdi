a70a4e734cd9a0dbe2d099b6850572165eb2ca13
nandiniperikala <nandini.p@comakeit.com>
2023-09-22 11:56:09 +0530

    batch script

--- generators/app/templates/minikube-auto-deploy.bat
+++ generators/app/templates/minikube-auto-deploy.bat
@@ -1,54 +1,61 @@
 @echo off
-setlocal enabledelayedexpansion
-
 REM Define the functions to call
 
 REM applicationDeployment
 :applicationDeployment
 echo.
 echo Prerequisites:
-echo  1. Make sure that you have installed kubectl and is configured
-echo  2. Make sure that you have installed minikube and make sure it's started.
-echo  3. Make sure that domain mapping is done properly.
+echo 1. Make sure that you have installed kubectl and is configured
+echo 2. Make sure that you have installed minikube and make sure it's started.
+echo 3. Make sure that domain mapping is done properly.
 echo.
 
-set /p "user_continue_action_appDeployment=Confirm if you meet all the above requirements (yes/no): "
-if "%user_continue_action_appDeployment%"=="yes" (
+set /p user_continue_action_appDeployment=Confirm if you meet all the above requirements (yes/no):
+
+if "%user_continue_action_appDeployment%" == "yes" (
     cd ..
     cd kubernetes
 
-    REM Replace the placeholder with minikube_ip 
-    for /f "delims=" %%i in ('kubectl get node -o jsonpath^='{.items[0].status.addresses[?(@.type=="InternalIP")].address}') do set "minikube_ip=%%i"
-
-    REM Replace minikube_ip_placeholder with minikube_ip
+    REM replace the placeholder with minikube_ip
+    for /f "tokens=*" %%i in ('kubectl get node -o jsonpath="{.items[0].status.addresses[?(@.type==\"InternalIP\")].address}"') do set "minikube_ip=%%i"
+    
+    REM replace pc with this minikube_ip
     if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-        (for /f "delims=" %%j in ('type K8S-README.md ^| find /v /n "" ^| find "minikube_ip_placeholder"') do (
-            set "line=%%j"
-            set "line=!line:~0,-24!!minikube_ip!"
-            echo(!line:*]=!
-        )) > K8S-README.md.new
-        move /y K8S-README.md.new K8S-README.md
-
-        (for /f "delims=" %%j in ('type kubectl-apply.sh ^| find /v /n "" ^| find "minikube_ip_placeholder"') do (
-            set "line=%%j"
-            set "line=!line:~0,-24!!minikube_ip!"
-            echo(!line:*]=!
-        )) > kubectl-apply.sh.new
-        move /y kubectl-apply.sh.new kubectl-apply.sh
+        for %%f in (K8S-README.md kubectl-apply.sh) do (
+            (for /f "delims=" %%a in (%%f) do (
+                echo %%a|findstr /i /c:"minikube_ip_placeholder">nul&&(
+                    (echo %%a|findstr /i /c:"minikube_ip_placeholder") || (
+                        (echo %%a|findstr /i /c:"minikube_ip_placeholder")>%%f.new
+                    )
+                )||(echo %%a)
+            )) && (copy /y %%f.new %%f)
+        )
     ) else (
-        (for /f "delims=" %%j in ('type K8S-README.md ^| find /v /n "" ^| find "minikube_ip_placeholder"') do (
-            set "line=%%j"
-            set "line=!line:~0,-24!!minikube_ip!"
-            echo(!line:*]=!
-        )) > K8S-README.md.new
-        move /y K8S-README.md.new K8S-README.md
+        (for /f "delims=" %%a in (K8S-README.md) do (
+            echo %%a|findstr /i /c:"minikube_ip_placeholder">nul&&(
+                (echo %%a|findstr /i /c:"minikube_ip_placeholder") || (
+                    (echo %%a|findstr /i /c:"minikube_ip_placeholder")>K8S-README.md.new
+                )
+            )||(echo %%a)
+        )) && (copy /y K8S-README.md.new K8S-README.md)
 
-        (for /f "delims=" %%j in ('type kubectl-apply.sh ^| find /v /n "" ^| find "minikube_ip_placeholder"') do (
-            set "line=%%j"
-            set "line=!line:~0,-24!!minikube_ip!"
-            echo(!line:*]=!
-        )) > kubectl-apply.sh.new
-        move /y kubectl-apply.sh.new kubectl-apply.sh
+        (for /f "delims=" %%a in (kubectl-apply.sh) do (
+            echo %%a|findstr /i /c:"minikube_ip_placeholder">nul&&(
+                (echo %%a|findstr /i /c:"minikube_ip_placeholder") || (
+                    (echo %%a|findstr /i /c:"minikube_ip_placeholder")>kubectl-apply.sh.new
+                )
+            )||(echo %%a)
+        )) && (copy /y kubectl-apply.sh.new kubectl-apply.sh)
+
+        cd keycloak-k8s
+
+        (for /f "delims=" %%a in (keycloak-configmap.yml) do (
+            echo %%a|findstr /i /c:"minikube_ip_placeholder">nul&&(
+                (echo %%a|findstr /i /c:"minikube_ip_placeholder") || (
+                    (echo %%a|findstr /i /c:"minikube_ip_placeholder")>keycloak-configmap.yml.new
+                )
+            )||(echo %%a)
+        )) && (copy /y keycloak-configmap.yml.new keycloak-configmap.yml)
     )
 
     cd ..
@@ -60,50 +67,52 @@ if "%user_continue_action_appDeployment%"=="yes" (
 )
 
 REM MINIKUBE
-:minikube
+:MINIKUBE
 echo You have opted for Minikube cluster
-REM Add code here to execute the action for minikube
 echo Initiating the minikube action....
 echo.
-set /p "user_continue_action=Continue with the Infrastructure deployment (yes/no): "
-if "%user_continue_action%"=="yes" (
-    echo Deploying the Infrastructure in our Minikube cluster.....
-    REM List of directories to process
-    set "directories=./eck ./istio ./istio-monitoring ./docker-build^&publish"
+set /p user_continue_action=Continue with the Infrastructure deployment (yes/no):
 
-    REM Loop through each directory and execute command
-    for %%d in (%directories%) do (
+if "%user_continue_action%" == "yes" (
+    echo Deploying the Infrastructure in our Minikube cluster.....
+    
+    REM List of directories to process
+    for %%d in (
+        "./eck"
+        "./istio"
+        "./istio-monitoring"
+        "./docker-build&publish"
+    ) do (
         if exist "%%d" (
             echo Processing directory: %%d
-            cd %%d
-
+            cd "%%d" || exit
+            
             REM Execute your command here
             echo Initiating terraform in %%d
             terraform init
             echo Auto applying terraform in %%d
             terraform apply -auto-approve
-
             cd ..
         ) else (
             echo Directory %%d does not exist
         )
     )
-
+    
     echo.
     echo Minikube Infrastructure was deployed successfully!
     echo.
 ) else (
     echo.
-    echo Looks like you have already deployed the WDI, Thank you!
+    echo Looks like you have already deployed the WDI, Thank you! 🐱
     echo.
 )
 
-set /p "appDeployment=Do you want to continue with the application Deployment (yes/no): "
+set /p appDepolyment=Do you want to continue with the application Deployment (yes/no):
 
-if "%appDeployment%"=="yes" (
-  call :applicationDeployment
+if "%appDepolyment%" == "yes" (
+    call :applicationDeployment
 ) else (
-  echo Thank you, user!
+    echo Thank you, user!
 )
 
 REM End of script
--- generators/app/templates/minikube-auto-destroy.bat
+++ generators/app/templates/minikube-auto-destroy.bat
@@ -1,17 +1,17 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
 REM Define the functions to call
 
 REM minikube
-:minikube
-echo You have opted for Minikube cluster
-REM Add code here to execute the action for Minikube
-echo Initiating the Minikube action...
+:MINIKUBE
+echo You have opted for minikube cluster
+REM Add code here to execute the action for minikube
+echo Initiating the minikube....
 echo.
-set /p "user_continue_action=Continue with destroying Infrastructure (yes/no): "
+set /p "user_continue_action=Continue with the destroying Infrastructure (yes/no): "
 if "%user_continue_action%"=="yes" (
-    echo Destroying the Infrastructure in our Minikube cluster...
+    echo Destroying the Infrastructure in our Minikube cluster.....
 
     REM List of directories to process
     set "directories=./istio-monitoring ./istio ./eck"
@@ -20,10 +20,9 @@ if "%user_continue_action%"=="yes" (
     for %%d in (%directories%) do (
         if exist "%%d" (
             echo Processing directory: %%d
-            cd %%d
-
+            cd %%d || exit /b
             REM Execute your command here
-            echo Auto destroying Terraform in %%d
+            echo Auto destroying terraform in %%d
             terraform destroy -auto-approve
             cd ..
         ) else (
@@ -32,13 +31,38 @@ if "%user_continue_action%"=="yes" (
     )
 
     echo.
-    echo Minikube Infrastructure was destroyed successfully! [insert appropriate emoji or message]
+    echo Minikube Infrastructure was destroyed successfully!^^
     echo.
-    echo [Insert any additional messages or instructions here]
+    echo Make sure you delete the ECR manually.^^
+    echo.
 ) else (
     echo.
-    echo Looks like you have already destroyed the Infrastructure. Thank you! [insert appropriate emoji or message]
+    echo Looks like you have already destroyed the Infrastructure. Thank you!^^
     echo.
 )
 
-REM End of script
+goto :EOF
+
+REM SCRIPT STARTS
+REM Check if figlet is installed
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    where figlet >nul 2>&1 || (
+        REM Install figlet using apt-get
+        echo Installing figlet...
+        sudo apt-get -y install figlet
+    )
+) else if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+    where figlet >nul 2>&1 || (
+        REM Install figlet using Homebrew
+        echo Installing figlet...
+        brew install figlet
+    )
+) else (
+    echo Unsupported architecture: %PROCESSOR_ARCHITECTURE%
+    exit /b 1
+)
+
+REM Use figlet to print "AutoDestroy"
+figlet -f slant "AutoDestroy"
+echo.
+call :MINIKUBE
