c0f077bc486edf0fab6d3383f850b70d3f5edaaa
craxkumar <90772223+craxkumar@users.noreply.github.com>
2023-10-31 16:26:58 +0530

    updated build image cmd in acr and docker (#41)

--- generators/app/templates/acr-build&publish/02-build-image.tf
+++ generators/app/templates/acr-build&publish/02-build-image.tf
@@ -28,7 +28,7 @@ resource "null_resource" "build_image" {
                 # Change back to the previous directory
                 cd ..
             elif [ -f "go.mod" ]; then
-                docker build --platform=linux/amd64 -t $(pwd | awk -F'/' '{print $(NF-1)}') .
+                docker build --platform=linux/amd64 -t $(pwd | awk -F'/' '{print $(NF)}') .
             else
                 # If the directory doesn't contain a "go" folder, run the following command
                 npm run java:docker
